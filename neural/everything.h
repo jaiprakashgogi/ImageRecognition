619661677cb9b4d738f4eddc79f9e259f7ab8926
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2015-12-11 18:47:47 -0500

    Split image into 6x6 patches

--- neural/everything.h
+++ neural/everything.h
@@ -11,3 +11,21 @@
 
 #include "knobs.h"
 
+/////////////////////////////////////////////////////////
+// Timing functions
+// Record the execution time of some code, in milliseconds.
+#define DECLARE_TIMING(s)  int64 timeStart_##s; double timeDiff_##s; double timeTally_##s = 0; int countTally_##s = 0
+#define START_TIMING(s)    timeStart_##s = cvGetTickCount()
+#define STOP_TIMING(s)     timeDiff_##s = (double)(cvGetTickCount() - timeStart_##s); timeTally_##s += timeDiff_##s; countTally_##s++
+#define GET_TIMING(s)      (double)(timeDiff_##s / (cvGetTickFrequency()*1000.0))
+#define GET_AVERAGE_TIMING(s)   (double)(countTally_##s ? timeTally_##s/ ((double)countTally_##s * cvGetTickFrequency()*1000.0) : 0)
+#define CLEAR_AVERAGE_TIMING(s) timeTally_##s = 0; countTally_##s = 0
+
+////////////////////////////////////////////////////////
+// Function prototypes
+
+// utils.cpp
+int getMemValue();
+
+// visualize.cpp
+cv::Mat visualize_patches(cv::Mat, cv::Mat);
--- neural/utils.cpp
+++ neural/utils.cpp
@@ -1,22 +1,25 @@
 #include "everything.h"    
 
-int parseLine(char* line){
+
+int parseLine(char* line) {
     int i = strlen(line);
-    while (*line < '0' || *line > '9') line++;
+    while (*line < '0' || *line > '9')
+        line++;
+
     line[i-3] = '\0';
+
     i = atoi(line);
     return i;
 }
 
-
-int getMemValue(){ //Note: this value is in KB!
+int getMemValue() { //Note: this value is in KB!
     FILE* file = fopen("/proc/self/status", "r");
     int result = -1;
     char line[128];
 
 
-    while (fgets(line, 128, file) != NULL){
-        if (strncmp(line, "VmSize:", 7) == 0){
+    while (fgets(line, 128, file) != NULL) {
+        if (strncmp(line, "VmSize:", 7) == 0) {
             result = parseLine(line);
             break;
         }
