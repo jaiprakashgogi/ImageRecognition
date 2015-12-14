f8887e1c61c8aeda668ad0d485adee06805171bd
Jai Prakash <jaiprakashgogi@gmail.com>
2015-12-14 16:34:35 -0500

    Whitening of 5K images

--- neural-matlab/test.m
+++ neural-matlab/test.m
@@ -1,7 +1,8 @@
 addpath('../vlfeat-0.9.20/toolbox/');
 run vl_setup.m;
 patch = [];
-for j = 1:1
+Data = [];
+for j = 1:5
     ws = sprintf('../data/small_data_batch_%d.mat', j);
     load(ws);
     for i = 1:size(data,1)
@@ -11,7 +12,18 @@ for j = 1:1
         toc
         patch = cat(1,patch, pat);
         %X = cat(1, X, x);
+        Data = cat(1, Data, data(i,:));
     end
     %[Xwh, mu, invMat, whMat] = whiten(patch);
-    %visualize_patch(patch, 6)
+    % visualize_patch(patch, 6)
+    % visualize_patch(data(i,:),patch(1+i*27*27:(i+1)*27*27,:), 6)
 end
+
+save('patch_1_5.mat', 'patch', 'Data');
+
+patch(find(isinf(patch))) = 0;
+patch(find(isnan(patch))) = 0;
+[Xwh, mu, invMat, whMat] = whiten(patch);
+
+% visualize_patch(data(i,:),patch(1+i*27*27:(i+1)*27*27,:), 6)
+% visualize_patch(data(i,:),Xwh(1+i*27*27:(i+1)*27*27,:), 6)
